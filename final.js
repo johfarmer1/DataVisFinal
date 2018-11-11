cc6f7886cc288ac4f53088a07e11d6138aef61e6
johfarmer1 <farmerj@carleton.edu>
2018-11-11 16:08:55 -0600

    create map thats zoomed in on florida

---
+++ final.js
@@ -0,0 +1,41 @@
+var width = 1000;
+var height = 600;
+
+var projection = d3.geo.albers()
+  .scale(1000)
+
+//.attr("transform", "translate(" + width / 2 + "," + height / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")")
+
+var geoPath = d3.geo.path()
+        .projection(projection);
+
+// Add svg and g elements to the webpage
+var svg = d3.select("#mapDiv").append("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .attr("transform", "translate(-650,-500)scale(4)")
+        .style('position', 'fixed');
+
+var buildMap = function() {
+  country = svg.selectAll('.country')
+      .data(geoData.features);
+  country
+      .enter()
+      .append('path')
+      .attr('class', 'country')
+      .attr('d', geoPath)
+      .style('fill', 'lightgrey')
+      .style('stroke', 'black')
+      ;
+};
+
+d3.json('USCounty.json', function(error, jsonData) {
+    geoData = jsonData;
+
+    // Next, let's load a CSV with economic data
+    // d3.csv('unhcr_refugee_data.csv', function(csvData) {
+    //     refugeeData = csvData;
+    // }
+
+    buildMap();
+});
