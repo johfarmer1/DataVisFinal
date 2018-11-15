88ae91d6e26bc79f12a55ff5d77b11f00f8a360b
Quinn <SchillerQuinn@gmail.com>
2018-11-15 17:41:22 -0600

    got zooming resizing to work

--- final.js
+++ final.js
@@ -1,14 +1,54 @@
-var width = 1000;
-var height = 600;
+//var width = 1200;
+//var height = 800;
 var votingMap;
+var scale0 = 8000
+var border = 1; // change to 0 to remove border
+var bordercolor = 'black';
 
-var projection = d3.geo.albers()
-  .scale(1000)
+var chartDiv = d3.select('#mapDiv').node()
+h = chartDiv.clientWidth - 33;
+w = chartDiv.clientHeight;
 
-//.attr("transform", "translate(" + width / 2 + "," + height / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")")
+function redraw() {
+  svg
+    .attr("width", 10)
+    .attr("height", 10);
 
-var geoPath = d3.geo.path()
-  .projection(projection);
+  borderPath
+    .attr("width", 10)
+    .attr("height", 10);
+  // Extract the width and height that was computed by CSS.
+  w = chartDiv.clientWidth - 33;
+  h = chartDiv.clientHeight;
+
+  // update sizes
+  svg
+    .attr("width", w)
+    .attr("height", h);
+
+  borderPath
+    .attr("width", w)
+    .attr("height", h);
+
+  projection
+    .scale(w*7)
+    .translate([w / 2, h / 2]);
+
+  svg.selectAll('.county').attr('d', path);
+};
+
+var projection = d3.geo.mercator()
+  .scale(scale0)
+  //.center([-93.75, 42.6]);
+  //.center([-93.69839, 42.209339])
+  //.center([-94.6, 41])
+  .center([-93.0977, 41.8780])
+  //41.8780° N, 93.0977°
+  .translate([w / 2, h / 2]);
+
+var path = d3.geo.path()
+  .projection(projection)
+
 
 var tip = d3.select('#mapDiv').append("tip")
   .attr('class', 'tooltip')
@@ -22,11 +62,18 @@ var tip = d3.select('#mapDiv').append("tip")
   .style('color', 'black');
 
 // Add svg and g elements to the webpage
-var svg = d3.select("#mapDiv").append("svg")
-  .attr("width", width)
-  .attr("height", height)
-  .attr("transform", "translate(-50,875)scale(8)")
-  .style('position', 'fixed');
+svg = d3.select("#mapDiv").append("svg")
+  .attr("border", border)
+
+//.style('position', 'fixed');
+borderPath = svg.append("rect")
+  .attr("x", 0)
+  .attr("y", 0)
+  .style("stroke", bordercolor)
+  .style("fill", "none")
+  .style("stroke-width", border);
+
+
 var districtColorMap = {
   '1': "red",
   '2': "green",
@@ -35,20 +82,20 @@ var districtColorMap = {
 }
 
 var buildMap = function() {
-  county = svg.selectAll('.county')
+  var county = svg.selectAll('.county')
     .data(geoData.features);
   county
     .enter()
     .append('path')
-    .attr('class', 'country')
-    .attr('d', geoPath)
+    .attr('class', 'county')
+    .attr('d', path)
     .style('stroke-width', '.3px')
     .style('stroke', 'black')
     .style('fill', function(d) {
       if (d.properties['STATE'] !== "19") {
         return 'grey';
       } else {
-        county = d.properties['NAME']
+        var county = d.properties['NAME']
         cDict = votingMap.get(county)
         district = cDict['District']
         return districtColorMap[district]
@@ -94,3 +141,9 @@ d3.csv('data/IowaCountyData.csv', function(csvData) {
     buildMap();
   });
 });
+
+// Draw for the first time to initialize.
+redraw();
+redraw();
+// Redraw based on the new size whenever the browser window is resized.
+window.addEventListener("resize", redraw);
